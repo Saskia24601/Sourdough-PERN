61a6e0dfb61f50be38ffc2bcc5396eacf7fb9f58
Saskia24601 <sarah.l.robinson93@gmail.com>
2025-06-09 12:00:02 +0100

    fialed to render react removed

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import React from "react";
 
 import productRoutes from "./routes/productRoute.js"; // Importing product routes
 import { sql } from "./config/db.js";
